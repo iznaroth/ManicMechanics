898c74060d178a4da965a9844bc4293f5d9d81da
iznaroth <iznaroth@gmail.com>
2022-12-02 05:30:26 -0600

    sealer GUI and recipe complete

---
+++ src/main/java/com/iznaroth/industrizer/recipe/ISealingChamberRecipe.java
@@ -0,0 +1,4 @@
+package com.iznaroth.industrizer.recipe;
+
+public class SealingChamberRecipe {
+}
---
+++ src/main/java/com/iznaroth/industrizer/recipe/IndustrizerRecipeTypes.java
@@ -0,0 +1,6 @@
+package com.iznaroth.industrizer.recipe;
+
+import net.minecraft.item.crafting.RecipeManager;
+
+public class IndustrizerRecipes extends RecipeManager {
+}
