7196a54879db8b1a085830516db61f956a9e47c9
iznaroth <iznaroth@gmail.com>
2022-12-12 06:33:43 -0600

    infuser work & complex recipe stuff

---
+++ src/main/java/com/iznaroth/manicmechanics/networking/packet/FluidSyncS2CPacket.java
@@ -0,0 +1,2 @@
+package com.iznaroth.manicmechanics.networking.packet;public class FluidSyncS2CPacket {
+}
---
+++ src/main/java/com/iznaroth/manicmechanics/recipe/InfuserRecipe.java
@@ -0,0 +1,130 @@
+package com.iznaroth.manicmechanics.recipe;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.iznaroth.manicmechanics.ManicMechanics;
+import com.iznaroth.manicmechanics.block.MMBlocks;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.GsonHelper;
+import net.minecraft.world.SimpleContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.*;
+import net.minecraft.world.level.Level;
+
+import javax.annotation.Nullable;
+
+public class SealingChamberRecipe implements Recipe<SimpleContainer> {
+
+    private final ResourceLocation id;
+    private final ItemStack output;
+    private final NonNullList<Ingredient> recipeItems;
+
+    public SealingChamberRecipe(ResourceLocation id, ItemStack output, NonNullList<Ingredient> recipeItems) {
+        this.id = id;
+        this.output = output;
+        this.recipeItems = recipeItems;
+    }
+
+    @Override
+    public boolean matches(SimpleContainer pContainer, Level pLevel) {
+
+        if(pLevel.isClientSide()) {
+            return false;
+        }
+
+        if(!recipeItems.get(0).test(pContainer.getItem(0))){
+            return false;
+        }
+
+        if(!recipeItems.get(1).test(pContainer.getItem(1))){
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public ItemStack assemble(SimpleContainer inv) {
+        return output;
+    }
+
+    @Override
+    public boolean canCraftInDimensions(int p_43999_, int p_44000_) {
+        return true;
+    }
+
+    @Override
+    public ItemStack getResultItem() {
+        return output.copy();
+    }
+
+    public ItemStack getIcon(){
+        return new ItemStack(MMBlocks.SEALER.get());
+    }
+
+    @Override
+    public ResourceLocation getId() {
+        return id;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return Serializer.INSTANCE;
+    }
+
+    @Override
+    public RecipeType<?> getType() {
+        return Type.INSTANCE;
+    }
+
+    public static class Type implements RecipeType<SealingChamberRecipe> {
+        private Type() { }
+        public static final Type INSTANCE = new Type();
+        public static final String ID = "sealer";
+    }
+
+    public static class Serializer implements RecipeSerializer<SealingChamberRecipe> {
+
+        public static final Serializer INSTANCE = new Serializer();
+        public static final ResourceLocation ID =
+                new ResourceLocation(ManicMechanics.MOD_ID, "sealer");
+
+        @Override
+        public SealingChamberRecipe fromJson(ResourceLocation recipeId, JsonObject json) {
+            ItemStack output = ShapedRecipe.itemStackFromJson(GsonHelper.getAsJsonObject(json, "output"));
+
+            JsonArray ingredients = GsonHelper.getAsJsonArray(json, "ingredients");
+            NonNullList<Ingredient> inputs = NonNullList.withSize(2, Ingredient.EMPTY);
+
+            for(int i = 0; i < inputs.size(); i++){
+                inputs.set(i, Ingredient.fromJson(ingredients.get(i)));
+            }
+
+            return new SealingChamberRecipe(recipeId, output, inputs);
+        }
+
+        @Nullable
+        @Override
+        public SealingChamberRecipe fromNetwork(ResourceLocation recipeId, FriendlyByteBuf buffer) {
+            NonNullList<Ingredient> inputs = NonNullList.withSize(2, Ingredient.EMPTY);
+
+            for(int i = 0; i < inputs.size(); i++){
+                inputs.set(i, Ingredient.fromNetwork(buffer));
+            }
+
+            ItemStack output = buffer.readItem();
+            return new SealingChamberRecipe(recipeId, output, inputs);
+        }
+
+        @Override
+        public void toNetwork(FriendlyByteBuf buffer, SealingChamberRecipe recipe) {
+            buffer.writeInt(recipe.getIngredients().size());
+            for(Ingredient ing : recipe.getIngredients()){
+                ing.toNetwork(buffer);
+            }
+            buffer.writeItemStack(recipe.getResultItem(), false);
+        }
+    }
+}
