452d30a7b6414018b8b76d2894f3c04973cbede2
iznaroth <iznaroth@gmail.com>
2023-02-14 01:22:31 -0600

    more button networking (branch conflict)

--- src/main/java/com/iznaroth/manicmechanics/blockentity/interfaces/IHasCyclableButton.java
+++ src/main/java/com/iznaroth/manicmechanics/blockentity/interfaces/IHasCyclableButton.java
@@ -5,4 +5,8 @@ public interface IHasButtonState {
     public void cycleForward(int which);
 
     public void cycleBackward(int which);
+
+    public void nonCyclePayload(int which, int payload){
+        
+    }
 }
